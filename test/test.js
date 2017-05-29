a6f520b7b2a6f9ba8c5105641ec838f4bca2552a
Michiel Mooring <michielmooring@gmail.com>
2017-05-29 16:33:21 +0200

    Added tests

--- test/test.js
+++ test/test.js
@@ -1,37 +1,84 @@
+const chai = require('chai')
+const expect = chai.expect
 const Client = require('../src/Client')
-const ytdl = require('ytdl-core')
+let client = null
+let client2 = null
 
-const client = new Client();
-
-client.on('ready', data => {
-    console.log(client.users)
-    // client.users.find('name', 'SuperUser').sendMessage('check')
-
-    client.sendMessage('Damn this works well')
-        .then(message => console.log(message))
-        .catch(err => console.log(err))
-
-    const stream = ytdl('https://www.youtube.com/watch?v=OpIQNxiKJoE')
-    client.voiceConnection.playStream(stream)
+before((done) => {
+    client = new Client()
+    client.on('ready', () => {
+        client2 = new Client({name: 'bob'})
+        client2.on('ready', () => {
+            done()
+        })
+    })
 })
 
-client.on('channelUpdate', (oldChannel, newChannel) => {
-    console.log(oldChannel)
-    console.log(newChannel)
+describe('TextMessage', () => {
+    it('should send a textmessage to current channel', () => {
+        return client.sendMessage('This is a test message')
+            .then(message => {
+                expect(message.content).to.equal('This is a test message')
+            })
+    })
+
+    it('should send a textmessage to a user', () => {
+        return client.users.find('name', 'bob').sendMessage('Hello')
+            .then(message => {
+                const user = message.users.find('name', 'bob')
+                expect(user.name).to.equal('bob')
+            })
+    })
+
+    it('should send a textmessage to a specific channel', () => {
+        return client.channels.find('name', 'Root')
+            .sendMessage('This is a specific channel message')
+            .then(message => {
+                const channel = message.channels.find('name', 'Root')
+                expect(channel.name).to.equal('Root')
+            })
+    })
+
+    it('should send a message recursive', () => {
+        return client.channels.find('name', 'Root')
+            .sendMessage('This is a recursive message', true)
+            .then(message => {
+                const channel = message.trees.find('name', 'Root')
+                expect(channel.name).to.equal('Root')
+            })
+    })
+
+    it('should reply to a textmessage from a user', (done) => {
+        client.on('message', (message) => {
+            message.reply('okay').then((reply) => {
+                const user = reply.users.find('name', 'bob')
+                expect(user.name).to.equal('bob')
+                expect(reply.content).to.equal('okay')
+                done()
+            })
+        })
+        client2.sendMessage('Reply to me')
+    })
 })
 
-client.on('channelCreate', channel => console.log(channel))
+describe('Channel', () => {
+    it('should list all channels', () => {
+        expect(client.channels.size).to.be.above(0)
+    })
 
-client.on('userChange', (oldUser, newUser) => {
-    console.log(oldUser)
-    console.log(newUser)
+    it('should find a specific channel', () => {
+        const channel = client.channels.find('name', 'Root')
+        expect(channel.name).to.equal('Root')
+    })
 })
 
-client.on('channelRemove', channelId => console.log(channelId))
+describe('User', () => {
+    it('should list all users', () => {
+        expect(client.users.size).to.be.above(1)
+    })
 
-client.on('message', message => {
-    message.reply("oke")
-        .then(message => console.log(message))
+    it('should find a specific user', () => {
+        const user = client.users.find('name', 'bob')
+        expect(user.name).to.equal('bob')
+    })
 })
-
-client.on('userDisconnect', user => console.log(user))
